40d76c0d5d2e2cd728deb6cea72a37433853ae36
AlcyZ <tobias_schindler@hotmail.de>
2022-02-01 01:12:57 +0100

    removed src backup (cause refactoring is finished :)

--- src_bak/hashing/mod.rs
+++
@@ -1,8 +0,0 @@
-use sha2::{Digest, Sha256};
-
-pub fn simple_sha256(data: impl AsRef<[u8]>) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(data);
-    let result = hasher.finalize();
-    format!("{:x}", result)
-}
